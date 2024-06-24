1e5650e4b27faff3842f17ea04caf7281a65aaef
Ricardo do Canto <endurancecode@gmail.com>
2024-06-24 21:24:09 +0100

    CPE: LAB - The print() function

---
+++ cisco-python-essentials/2-1-12-lab-print-function.py
@@ -0,0 +1,10 @@
+# Initial state
+print("Programming","Essentials","in")
+print("Python")
+
+# Expected output:
+# Programming***Essentials***in...Python
+
+# Final state
+print("Programming","Essentials","in", sep="***", end="...")
+print("Python")
