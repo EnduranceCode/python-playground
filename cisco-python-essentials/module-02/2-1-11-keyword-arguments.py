c43ad2835933943d9cfc097877967de5e4dcdc4b
Ricardo do Canto <endurancecode@gmail.com>
2024-06-24 21:17:29 +0100

    CPE: Keyword arguments

---
+++ cisco-python-essentials/module-02/2-1-11-keyword-arguments.py
@@ -0,0 +1,15 @@
+# First case
+print("My name is", "Python.", end=" ")
+print("Monty Python.")
+
+
+# Second case
+print("My name is ", end="")
+print("Monty Python.")
+
+# Third case
+print("My", "name", "is", "Monty", "Python.", sep="-")
+
+# Fourth case
+print("My", "name", "is", sep="_", end="*")
+print("Monty", "Python.", sep="*", end="*\n")
