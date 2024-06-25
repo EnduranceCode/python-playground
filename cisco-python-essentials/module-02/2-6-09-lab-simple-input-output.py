02220addd63759d88777a381d6551402f4513b28
Ricardo do Canto <endurancecode@gmail.com>
2024-06-25 21:26:53 +0100

    CEP: LAB - Simple input and output

---
+++ cisco-python-essentials/module-02/2-6-09-lab-simple-input-output.py
@@ -0,0 +1,15 @@
+# input a float value for variable a here
+first_value = float(input("Input first numeric value: "))
+# input a float value for variable b here
+second_value = float(input("Input second numeric value: "))
+
+# output the result of addition here
+print("Adition value:", first_value + second_value)
+# output the result of subtraction here
+print("Subtraction value:", first_value - second_value)
+# output the result of multiplication here
+print("Multiplicatio value:", first_value * second_value)
+# output the result of division here
+print("Division value:", first_value / second_value)
+
+print("\nThat's all, folks!")
